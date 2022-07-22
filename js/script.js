614138c8cff6295a2e254a789be50486e7fd21c8
Arkady <75026162+Ark235@users.noreply.github.com>
2022-07-22 20:58:49 +0500

    Add function addSquares16. It fills drawing field with squares 20px*20px

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-createSingleSquare();
+addSquares16();
 
-/** Add single square to drawing field */
+//** Control buttons */
+
+const clearBtn = document.querySelector('.clear');
+const multicolorBtn = document.querySelector('.multicolor');
+const monochromeBtn = document.querySelector('.monochrome');
+
+//** Add single square 20px*20px to drawing field */
 
 function createSingleSquare() {
     const container = document.querySelector('.drawing-field');
     const singleSquare = document.createElement('div');
-    singleSquare.setAttribute('style', 'width: 20px; height: 20px; box-sizing: border-box; border: 1px solid blue;');
+    singleSquare.setAttribute('style', 'width: 20px; height: 20px; box-sizing: border-box; border: 1px solid rgba(8, 7, 7, 0.2);');
     container.appendChild(singleSquare);
+}
+
+//** Fill drawing field 16x16 with squares 20px*20px */
+
+function addSquares16() {
+    for (let i = 0; i < 256; i++) {
+        createSingleSquare();
+    }
 }
